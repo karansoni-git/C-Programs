a6cccce6db1a88c675de7f1472c96f323ebf477c
karan soni <karanparekh1011@gmail.com>
2025-02-18 22:21:59 +0530

    Add files via upload

---
+++ DATATYPE.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int no=10;
+char ch='a';
+char str[]="hello world";
+float flt=10.10;
+printf("int:%d\n",no);
+printf("single char:%c\n",ch);
+printf("string:%s\n",str);
+printf("float:%f\n",flt);
+getch();
+return 0;
+}
