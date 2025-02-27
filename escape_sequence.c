e8a01973c34c03623529e4b44b0b28eb45804d28
karan soni <karanparekh1011@gmail.com>
2025-02-27 22:48:54 +0530

    Add files via upload

---
+++ escape_sequence.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	// \a:- give an alert bell by system 
+//	printf("hello karan\a");
+	
+	// \b:- single bit back of cursor
+//	printf("hello all\b"); 
+	
+	// /n:- cursor jump to the new line
+//	printf("hello all\nheyy");
+
+	// \t:- horizontal tab of 8 bits
+//	printf("hello all\n");
+//	printf("hello\tall\n");
+//	printf("hello   \tall");  //it combine with the and complete the 8 bit
+
+	// \r:- cursor goto the begining also can write 
+//	printf("hello all\r");
+
+	// \v:- give vertical space between two lines
+//	printf("hello\n\v");
+//	printf("all");
+
+	printf("hello\n");
+	printf("\fall");
+	getch();
+	return 0;
+}
