19c722eebb517bb1cbd7bcc574a311519d4ab76b
karan soni <karanparekh1011@gmail.com>
2025-03-07 19:29:23 +0530

    Add files via upload

---
+++ PRIME_NUMBER.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,c=0,i;
+printf("enter number:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+c++;
+}
+}
+if(c==2)
+{
+printf("prime");
+}
+else
+{
+printf("not prime");
+}
+getch();
+return 0;
+}
